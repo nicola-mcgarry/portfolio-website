df567e22cd39794abf44b50e7657b13e1a65162e
nicolamcgarry <nicola.mcgarry@btinternet.com>
2024-07-08 11:52:21 +0100

    netlify contact form

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -1,63 +1,79 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { TextField, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
+import React from 'react';
+// import React, { useState } from 'react';
+// import axios from 'axios';
+// import { TextField, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
 
-const apiUrl = import.meta.env.VITE_API_URL;
+// const apiUrl = import.meta.env.VITE_API_URL;
 
 const ContactForm: React.FC = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: '',
-  });
+  // const [formData, setFormData] = useState({
+  //   name: '',
+  //   email: '',
+  //   message: '',
+  // });
 
-  const [dialogOpen, setDialogOpen] = useState(false);
-  const [dialogMessage, setDialogMessage] = useState('');
-  const [dialogTitle, setDialogTitle] = useState('');
+  // const [dialogOpen, setDialogOpen] = useState(false);
+  // const [dialogMessage, setDialogMessage] = useState('');
+  // const [dialogTitle, setDialogTitle] = useState('');
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const { name, value } = e.target;
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [name]: value,
-    }));
-  };
+  // const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  //   const { name, value } = e.target;
+  //   setFormData((prevFormData) => ({
+  //     ...prevFormData,
+  //     [name]: value,
+  //   }));
+  // };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  // const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
 
-    if (!formData.name || !formData.email || !formData.message) {
-        setDialogTitle('Error');
-        setDialogMessage('All fields are required.');
-        setDialogOpen(true);
-        return;
-      }
+  //   if (!formData.name || !formData.email || !formData.message) {
+  //       setDialogTitle('Error');
+  //       setDialogMessage('All fields are required.');
+  //       setDialogOpen(true);
+  //       return;
+  //     }
 
-    axios.post(`${apiUrl}/send-contact-form`, formData)
-      .then(() => { 
-        setDialogTitle('Success');
-        setDialogMessage('Message sent successfully!');
-        setDialogOpen(true);
-      })
-      .catch(() => { 
-        setDialogTitle('Error');
-        setDialogMessage('An error occurred while sending the message.');
-        setDialogOpen(true);
-      });
-  };
+  //   axios.post(`${apiUrl}/send-contact-form`, formData)
+  //     .then(() => { 
+  //       setDialogTitle('Success');
+  //       setDialogMessage('Message sent successfully!');
+  //       setDialogOpen(true);
+  //     })
+  //     .catch(() => { 
+  //       setDialogTitle('Error');
+  //       setDialogMessage('An error occurred while sending the message.');
+  //       setDialogOpen(true);
+  //     });
+  // };
 
-  const handleClose = () => {
-    setDialogOpen(false);
-    setFormData({
-        name: '',
-        email: '',
-        message: '',
-      });
-  };
+  // const handleClose = () => {
+  //   setDialogOpen(false);
+  //   setFormData({
+  //       name: '',
+  //       email: '',
+  //       message: '',
+  //     });
+  // };
 
   return (
     <>
-      <form onSubmit={handleSubmit} data-netlify={"true"}>
+      <form name="contact" method="POST" data-netlify="true">
+        <p>
+          <label>Your Name: <input type="text" name="name" /></label>
+        </p>
+        <p>
+          <label>Your Email: <input type="email" name="email" /></label>
+        </p>        
+        <p>
+          <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+ 
+      {/* <form onSubmit={handleSubmit} data-netlify={"true"}>
         <div>
           <TextField
             label="Name"
@@ -113,7 +129,7 @@ const ContactForm: React.FC = () => {
             Close
           </Button>
         </DialogActions>
-      </Dialog>
+      </Dialog> */}
     </>
   );
 };
