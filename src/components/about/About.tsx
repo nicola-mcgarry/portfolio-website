f1f32553adaea22d6ccb1d306e379eb84dc2c58d
nicolamcgarry <nicola.mcgarry@btinternet.com>
2024-07-09 22:10:52 +0100

    about width

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -12,9 +12,9 @@ import { FaAngular } from "react-icons/fa6";
 const About = () => {
     return(
         <div id="about" className='container d-flex flex-column'>
-            <div className='row ms-4 me-4'>
-                <div className='col-md-12 col-sm-12 d-flex justify-content-center'>
-                    <div className='p-4 '>
+            <div className='row'>
+                <div className='col-md-12 d-flex justify-content-center'>
+                    <div className='p-4'>
                             <p>Hi there! I'm a Front End Developer with 5 years of commercial experience. My coding journey began as a self-taught enthusiast, and in 2019, I was fortunate enough to land an apprenticeship that solidified my passion for software development. Although I've been trained in full stack development, my heart is set on front end, and that's where I'm focusing my skills.</p>
                             <p>Thanks to my full stack background, I bring a unique perspective to front end development, ensuring that my projects are not only beautiful but also efficient and user-friendly. I'm always excited to learn new things and keep up with the latest trends and technologies in the industry.</p>
                             <p>When I'm not coding, you can find me enjoying a good cup of coffee, gaming, or hitting the gym. Personal development is super important to me, so I'm always looking for ways to grow both professionally and personally. I'm excited about the future and can't wait to see what new challenges and opportunities lie ahead!</p>               
